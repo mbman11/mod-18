0e6df0895638a82b99382efa9c02ca20e8c676a5
mbman11 <m.behrmann11@gmail.com>
2023-10-29 22:56:53 -0600

    added routes, conections, and more for functionality

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const user = require('./user');
-const thought = require('./thoughts');
+const User = require('./User');
+const Thought = require('./Thought');
 // Exporting the User and Thought models as a single module for easy access in other parts of the application
-module.exports = {thoughts, user};
+module.exports = { Thought, User };
